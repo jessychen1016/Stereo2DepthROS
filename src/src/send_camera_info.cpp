b0f9aee92b8d31c638f5760dea85b1f22ad0eaba
jessychen1016 <chenzexi1016@126.com>
2019-11-25 23:34:41 +0800

    tuned the topic but couldnt work yet

---
+++ src/src/send_camera_info.cpp
@@ -0,0 +1,62 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "sensor_msgs/CameraInfo.h"
+#include <sstream>
+
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "CameraInfoPublisher");
+
+
+  ros::NodeHandle n;
+
+  ros::Publisher pubCameraInfoLeft = n.advertise<sensor_msgs::CameraInfo>("/left/camera_info", 1);
+  ros::Publisher pubCameraInfoRight = n.advertise<sensor_msgs::CameraInfo>("/right/camera_info", 1);
+
+  ros::Rate loop_rate(100);
+
+  while (ros::ok())
+  {
+    /**
+     * This is a message object. You stuff it with data, and then publish it.
+     */
+    sensor_msgs::CameraInfo cam_info_left;
+    sensor_msgs::CameraInfo cam_info_right;
+
+    cam_info_left.header.frame_id = 100;
+    cam_info_left.header.seq = 100;
+    cam_info_left.header.stamp.sec = 157379094;
+    cam_info_left.header.stamp.nsec = 976513000;
+    cam_info_left.width = 640;
+    cam_info_left.height = 480;
+    cam_info_left.K = {577.7599422834412,  0, 377.77733143404856, 0, 576.4409819801004, 273.33279803110605, 0,  0,  1};
+    cam_info_left.D = {-0.07510631419563545, 0.09363042700964139, 0.008738068640747354, 0.001945801963267056};
+
+    cam_info_right.header.frame_id = 100;
+    cam_info_right.header.seq = 100;
+    cam_info_right.header.stamp.sec = 157379094;
+    cam_info_right.header.stamp.nsec = 976513000;
+    cam_info_right.width = 640;
+    cam_info_right.height = 480;
+    cam_info_right.K = {596.3538688978191,  0, 370.4700065798576, 0, 597.5464430438726, 272.15617834392657, 0,  0,  1};
+    cam_info_right.D = {-0.058433024732107014, 0.07946394064467827, 0.006107121258570722, -0.003165480863601939};
+
+
+    /**
+     * The publish() function is how you send messages. The parameter
+     * is the message object. The type of this object must agree with the type
+     * given as a template parameter to the advertise<>() call, as was done
+     * in the constructor above.
+     */
+    pubCameraInfoLeft.publish(cam_info_right);
+    pubCameraInfoRight.publish(cam_info_left);    
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+  }
+
+
+  return 0;
+}
